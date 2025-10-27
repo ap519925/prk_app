0ae5489a97d97cc691df56b1abb7c29931ce2b58
Anthony Phillips <a.phillips@comsoc.org>
2025-10-27 08:02:57 -0400

    Add Google Maps API key and configure Android/iOS platforms with permissions

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,17 @@
+import Flutter
+import UIKit
+import GoogleMaps
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Google Maps API Key
+    GMSServices.provideAPIKey("AIzaSyCmdngbXx6VBraDfM3-NgKbA0q7DAjcl3Q")
+    
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
